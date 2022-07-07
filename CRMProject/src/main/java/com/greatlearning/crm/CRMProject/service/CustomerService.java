b1cc92babb2a86663b64a0a244b21bb2c092cb90
Pavith Sudhir <56886753+PavithSudhirAP@users.noreply.github.com>
2022-07-07 23:53:59 +0530

    Add files via upload

---
+++ CRMProject/src/main/java/com/greatlearning/crm/CRMProject/service/CustomerService.java
@@ -0,0 +1,5 @@
+package com.greatlearning.crm.CRMProject.service;
+
+public interface CustomerService {
+
+}
---
+++ CRMProject/src/main/java/com/greatlearning/crm/CRMProject/service/CustomerServiceImpl.java
@@ -0,0 +1,5 @@
+package com.greatlearning.crm.CRMProject.service;
+
+public class CustomerServiceImpl implements CustomerService {
+
+}
