28a5c6e6451a798564cb53448557b7e37c64ff84
Pavith Sudhir <56886753+PavithSudhirAP@users.noreply.github.com>
2022-07-10 11:42:56 +0530

    Add files via upload

---
+++ src/main/java/com/gl/javafsd/crmproject/controller/CustomerController.java
@@ -0,0 +1,73 @@
+package com.gl.javafsd.crmproject.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.gl.javafsd.crmproject.entity.Customer;
+import com.gl.javafsd.crmproject.service.CustomerService;
+
+@Controller
+@RequestMapping("/customer")
+public class CustomerController {
+
+	@Autowired
+	private CustomerService customerService;
+
+	@RequestMapping("/customerAdd")
+	public String handleBeginAdd(Model theModel) {
+
+		Customer student = new Customer();
+
+		theModel.addAttribute("student", student);
+
+		return "customer-details";
+	}
+
+	@RequestMapping("/customerUpdate")
+	public String handleBeginUpdate(
+			@RequestParam("customerId") int theId, 
+			Model theModel) {
+
+		Customer customer = customerService.findById(theId);
+		theModel.addAttribute("customer", customer);
+		return "customer-details";
+	}
+
+	@PostMapping("/save")
+	public String handleSave(
+		@RequestParam("id") int id, 
+		@RequestParam("firstName") String firstName,
+		@RequestParam("lastName") String lastName, 		
+		@RequestParam("email") String email) {
+
+		Customer customer;
+		if (id != 0) {
+			
+			customer = customerService.findById(id);
+			customer.setFirstName(firstName);
+			customer.setLastName(lastName);
+			customer.setEmail(email);
+		} else {
+		
+			customer = new Customer(firstName, lastName, email);
+		}	
+		customerService.save(customer);
+		return "redirect:/customer/list";
+
+	}
+
+	@RequestMapping("/delete")
+	public String handleDelete(@RequestParam("customerId") int theId) {
+
+		customerService.deleteById(theId);
+
+		return "redirect:/customer/list";
+
+	}
+
+}
